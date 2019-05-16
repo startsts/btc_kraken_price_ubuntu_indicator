d6ef176be44af56da8a30534868e41e55936147c
alex90aaa <startsts@yandex.ru>
2019-05-16 19:01:17 -0300

    refactor, gitignore for nohup

--- main.py
+++ main.py
@@ -10,20 +10,20 @@ def main():
   gtk.main()
 def menu():
   menu = gtk.Menu()
- 
   exittray = gtk.MenuItem('Exit Tray')
   exittray.connect('activate', quit)
   menu.append(exittray)
-  
   menu.show_all()
   return menu
-  
-def quit(_):
-  gtk.main_quit()
 def change_label(ind_app):
+  try:
     j = requests.get("https://api.kraken.com/0/public/Ticker?pair=XXBTZUSD")
     ind_app.set_label(str(round(float(j.json()["result"]['XXBTZUSD']["a"][0]),2)) , '')
-    return True
+  except:
+    pass
+  return True
+def quit(_):
+  gtk.main_quit()
 if __name__ == "__main__":
   main()
 
