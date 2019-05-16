f743427c21e71870863e96cf01237c01256e0805
alex90aaa <startsts@yandex.ru>
2019-05-16 15:01:50 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import os, requests
+from gi.repository import Gtk as gtk, AppIndicator3 as appindicator, GLib
+def main():
+  iconpath = "/home/alex90/btc_price_kraken/icon.png"
+  indicator = appindicator.Indicator.new("customtray", iconpath, appindicator.IndicatorCategory.APPLICATION_STATUS)
+  indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+  indicator.set_menu(menu())
+  change_label(indicator)
+  GLib.timeout_add(5000, change_label, indicator)
+  gtk.main()
+def menu():
+  menu = gtk.Menu()
+ 
+  exittray = gtk.MenuItem('Exit Tray')
+  exittray.connect('activate', quit)
+  menu.append(exittray)
+  
+  menu.show_all()
+  return menu
+  
+def quit(_):
+  gtk.main_quit()
+def change_label(ind_app):
+    j = requests.get("https://api.kraken.com/0/public/Ticker?pair=XXBTZUSD")
+    ind_app.set_label(str(round(float(j.json()["result"]['XXBTZUSD']["a"][0]),2)) , '')
+    return True
+if __name__ == "__main__":
+  main()
+
